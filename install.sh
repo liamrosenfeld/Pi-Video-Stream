a03b9cbea51b9a65f897fef02e8bfbb60096e0e5
Liam Rosenfeld <liamrosenfeld@gmail.com>
2018-05-04 23:19:28 -0500

    Added Saving of Settings to config.txt

--- install.sh
+++ install.sh
@@ -1,5 +1,48 @@
 #!/bin/bash
 
-echo -n "Enter your ngrok token and press [ENTER]: "
-read token
-./ngrok authtoken $token
+# Functions to Reduce Redundancy
+confirm() {
+    # call with a prompt string or use a default
+    read -r -p "${1:-Are you sure? [y/N]} " response
+    case "$response" in
+        [yY][eE][sS]|[yY]) 
+            true
+            ;;
+        *)
+            false
+            ;;
+    esac
+}
+
+writeToConfig() {
+	# $1 = Find and $2 = Replace
+	sed -i "s/$1/$2/g" $config
+}
+
+# Setup Config File
+config="./config.txt"
+if [ ! -e "$config" ]; then
+	echo "Creating Config File..."
+	touch "$config"
+	echo "Config File Created"
+else
+	echo "Config file already present"
+	if confirm "Do you want to reconfigure? (Y/n):"; then
+		cat /dev/null > $config
+		echo "Config file reset"
+	else 
+		exit 0
+	fi
+fi
+
+# Setup Ngrok
+if confirm "Do you want to use ngrok? (Y/n):"; then 
+	echo "useNgrok=true" >> $config
+	echo -n "Please enter your ngrok token: "
+	read token
+	./ngrok authtoken "$token"
+else 
+	echo "useNgrok=false" >> $config
+fi
+    
+
--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-# Setup Flask App
+config="./config.txt"
+
+# Start Flask App
 python app.py &
 
-# Set Up Ngrok
-./ngrok http 8080 &
-curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p'
-sleep 10
+# Start Ngrok
+temp=$(awk '/^useNgrok/{print $NF}' $config)
+IFS="=" read name value <<< "$temp"
+if [[ "$value" == "true" ]]; then
+	./ngrok http 8080 &
+	url = $(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"https:..([^"]*).*/\1/p')
+fi
