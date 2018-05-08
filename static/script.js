ac96f17480c7cff2c12f1cb73eb9db0286936ee8
Liam Rosenfeld <liamrosenfeld@gmail.com>
2018-05-07 19:36:56 -0500

    Added Fullscreen Mode

--- static/script.js
+++ static/script.js
@@ -2,9 +2,19 @@ function optionBox(action) {
 	if (action == "open") {
 		document.getElementById('light').style.display='block';
 		document.getElementById('fade').style.display='block';
-	}
-	if (action == "close") {
+	} else if (action == "close") {
 		document.getElementById('light').style.display='none';
 		document.getElementById('fade').style.display='none';
 	}
 }
+
+function resizeVideo(size) {
+	var video = document.getElementById("video");
+	
+	if (size == "fullscreen" && !video.classList.contains("fullscreen")) {
+		video.className += "fullscreen";
+	} else if (size == "real") {
+		video.classList.remove("fullscreen"); 
+	}
+}
+
